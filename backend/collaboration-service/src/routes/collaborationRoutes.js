b237aa4bf0902bc8d794c41cd15ad14f419103c7
yash0001 <115033773+yashmathuria0001@users.noreply.github.com>
2025-03-11 01:24:08 +0530

    issue resolved

--- backend/collaboration-service/src/routes/collaborationRoutes.js
+++ backend/collaboration-service/src/routes/collaborationRoutes.js
@@ -2,8 +2,8 @@ import express from "express";
 import { getDocument, saveDocument ,createDocument} from "../controllers/collaborationController.js";
 
 const router = express.Router();
-
+router.post("/create-doc",createDocument);
 router.get("/:id", getDocument);
 router.post("/:id", saveDocument);
-router.post("/create-doc",createDocument)
+
 export default router;
